a837c94e3bffcf6c32ddce5c1ca3e9c390cd2ecb
rabbialamph <mdrabbialom98@gmail.com>
2026-03-20 11:46:28 +0600

    implement login page with javascript

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ document.getElementById('login-btn').addEventListener('click', () => {
     const number = document.getElementById('input-number').value;
     const pin = document.getElementById('input-pin').value;
 
-    if(number === '01234567899' && pin === '1234'){
+    if(number === '01322334455' && pin === '1234'){
         window.location.assign('home.html');
     }else{
         alert('Invalid Number or Pin')
